6bfab2d55b904ef2eac14946ef76afaa2689fca0
Jungman Bae <jmbae@liv.kr>
2024-07-22 10:13:50 +0900

    Initialize NoteApp

---
+++ NoteApp/NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Jungman Bae on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
