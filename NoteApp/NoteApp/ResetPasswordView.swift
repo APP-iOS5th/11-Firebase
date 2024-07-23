099bab2c0b4a9edaae68b2de5329807942aeebb3
Jungman Bae <jmbae@liv.kr>
2024-07-23 10:44:16 +0900

    SignUpView 추가

---
+++ NoteApp/NoteApp/ResetPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordView.swift
+//  NoteApp
+//
+//  Created by Jungman Bae on 7/23/24.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ResetPasswordView()
+}
