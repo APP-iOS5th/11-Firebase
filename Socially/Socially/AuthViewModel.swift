115d07e89149a15c4f3bff2ad87287702d91327c
Jungman Bae <jmbae@liv.kr>
2024-07-23 15:44:34 +0900

    페이지 이동로직 추가

--- Socially/Socially/AuthViewModel.swift
+++ Socially/Socially/AuthViewModel.swift
@@ -14,6 +14,20 @@ import CryptoKit
 class AuthViewModel: ObservableObject {
     @Published var user: User?
     var currentNonce: String?
+    
+    func listenToAuthState() {
+        Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            self?.user = user
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 
     // MARK: - Sign in with Apple Methods
     func signInWithApple(request: ASAuthorizationAppleIDRequest) {
