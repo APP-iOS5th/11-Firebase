c21894b4c8e84e80c6dd977b8e2cffd83a243992
Jungman Bae <jmbae@liv.kr>
2024-07-24 14:46:26 +0900

    썸네일 URL 추가(오류있음)

--- Socially/Socially/Posts/ViewModels/PostViewModel.swift
+++ Socially/Socially/Posts/ViewModels/PostViewModel.swift
@@ -13,15 +13,28 @@ class PostViewModel: ObservableObject {
     private var databaseReference = Firestore.firestore().collection("Posts")
     
     func addData(description: String, datePublished: Date, data: Data, completion: @escaping (Error?) -> Void) {
-        let storageReference = Storage.storage().reference().child("\(UUID().uuidString)")
+        let fileUUID = UUID().uuidString
+        let storageReference = Storage.storage().reference().child("\(fileUUID)")
 
-        storageReference.putData(data, metadata: nil) { metadata, error in
+        // 파일 타입 및 MIME 타입 감지
+        let mimeType = detectMimeType(from: data)
+        
+        // 메타데이터 설정
+        let metadata = StorageMetadata()
+        metadata.contentType = mimeType
+        
+        storageReference.putData(data, metadata: metadata) { metadata, error in
             if let error = error {
                 completion(error)
                 return
             }
             
-            storageReference.downloadURL { url, error in
+            // 임의로 10초 대기 ( 썸네일 생성 시간 )
+            Thread.sleep(forTimeInterval: 10)
+            
+            let thumbRef = Storage.storage().reference().child("thumbs/\(fileUUID)_320x200")
+
+            thumbRef.downloadURL { url, error in
                 if let error = error {
                     completion(error)
                     return
@@ -42,4 +55,30 @@ class PostViewModel: ObservableObject {
             }
         }
     }
+    
+    func detectMimeType(from data: Data) -> String {
+        var c: UInt8 = 0
+        data.copyBytes(to: &c, count: 1)
+        
+        switch c {
+        case 0xFF:
+            return "image/jpeg"
+        case 0x89:
+            return "image/png"
+        case 0x47:
+            return "image/gif"
+        case 0x49, 0x4D:
+            return "image/tiff"
+        case 0x25:
+            return "application/pdf"
+        case 0xD0:
+            return "application/vnd"
+        case 0x46:
+            return "text/plain"
+        default:
+            // 기본값으로 application/octet-stream 사용
+            return "application/octet-stream"
+        }
+    }
+
 }
