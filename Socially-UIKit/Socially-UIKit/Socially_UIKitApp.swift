e1ddb42cc91b3df6c41b8a2d42cd78379913b59d
Jungman Bae <jmbae@liv.kr>
2024-07-25 09:39:58 +0900

    Initailize Socially-UIKit

---
+++ Socially-UIKit/Socially-UIKit/Socially_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Socially_UIKitApp.swift
+//  Socially-UIKit
+//
+//  Created by Jungman Bae on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Socially_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
