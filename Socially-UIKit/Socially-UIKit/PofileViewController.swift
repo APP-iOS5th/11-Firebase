5601de65badf2891a0ec4c6fc35a1ade7736d715
Jungman Bae <jmbae@liv.kr>
2024-07-26 09:26:27 +0900

    스토리보드에 프로필뷰 추가

---
+++ Socially-UIKit/Socially-UIKit/PofileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PofileViewController.swift
+//  Socially-UIKit
+//
+//  Created by Jungman Bae on 7/26/24.
+//
+
+import UIKit
+
+class PofileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Profile"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 0)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
